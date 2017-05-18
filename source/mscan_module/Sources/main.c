0ead53c930f62695505d7d605be0084a9479c1f7
Dinkar Sharma <Dsharma4187@conestogac.on.ca>
2017-05-18 11:50:10 -0400

    Updated MSCAN module.

--- source/mscan_module/Sources/main.c
+++ source/mscan_module/Sources/main.c
@@ -1,61 +1,99 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
 #include <hidef.h>      /* common defines and macros */
 #include "derivative.h"      /* derivative-specific definitions */
 #include "mc9s12c32.h"
 #include "mscan.h"
 
 
-void main () {   
- unsigned char errorflag = NO_ERR;   
- unsigned char txbuff[] = "ABCDEFGH";
- 
- CANInit();   
- while (!(CAN0CTL0&0x10));   
- CAN0RFLG = 0xC3;   
- CAN0RIER = 0x01;   
- while ((CAN0CTL1&0x00) != 0) {} /* Wait for Normal Mode */
- 
- Enable Interrupts;   
-  
- for (;;) {        
-    errorflag = CAN0SendFrame((ST_ID_100), 0x00, sizeof(txbuff)-1, txbuff);        
-    Delay();  
-  }
+unsigned char CANSendFrame(unsigned long id, unsigned char priority, unsigned char length, unsigned char *txdata);
+void CANInit(void);
+void delay(int milliseconds);
 
-}
-
-void CANInit (void)
-CAN0CTL0 = 0x01; /* Enter Initialization Mode */
-while (! (CANOCTL1&0x01)) {}; /* Wait for Initialization Mode */
-                              /* acknowledge (INITRQ bit = 1) */
- 
-unsigned char CAN0SendFrame *unsigned log id, _unsigned char priority, _unsigned char length, _unsigned char *txdata )
-{ 
-  if (!CAN0TFLG) /* Is Transmit Buffer full?? */
-    return ERR_BUFFER_FULL;
-  CAN0TBSEL = CANOTFLG; /* Select lowest empty buffer */
-  txbuffer = CAN0TBSEL; /* Backup selected buffer */
- 
-}
-
-/* Load Id to IDR Register */
-  *((unsigned long *) ((unsigned long)(&CAN0TXIDR0))) = id;
-  for (index=0;index<length;index++) {
-    *(&CAN0TXDSR0 + index) = txdata[index]; /* Load data to Tx buffer
-     *Data Segment Registers*/
-  }
- 
- 
- CAN0TXDLR = length; /* Set Data Length Code */
- CAN0TXTBPR = priority; /* Set Priority */
- CAN0TFLG = txbuffer; /* Start transmission */
- while ( (CAN0TFLG & txbuffer) != txbuffer); /* Wait for Transmission */
- /* completion*/
- 
- void interrupt CAN0RxISR(void)
+void CANInit(void)
 {
- unsigned char length, index;
- unsigned char rxdata[8];
- length = {CAN0RXDLR & 0x0F);
- for (index=0; index<length; index++)
- rxdata[index] = *(&CAN0RXDSR0 + index); /* Get received data */
- CAN0RFLG = 0x01; /* Clear RXF */
+  SET_BITS(CANCTL0, 0x01); // enter initialization mode
+  while(!(CANCTL1&0x01)){}; // initilization acknowledgment
+  SET_BITS(CANCTL1, 0x40); // enable bus clock 8MHz
+  SET_BITS(CANBTR0, 0x07); // set prescaler k = 8
+  SET_BITS(CANBTR1, 0x23); // set N = 8, TEG1 = 4, TSEG2 = 3, TQ = 1
+  CANCTL0 = 0x00; // exit init mode
+  while((CANCTL1&0x00) != 0) {}; // wait for normal mode
+}
+void main()
+{
+  unsigned char errorflag = NO_ERR;
+  unsigned char txbuff[] = "ABCDEFGH";
+  
+  CANInit();
+  while(!(CANCTL0&0x10)); 
+  
+  CANRFLG = 0xC3;
+  CANRIER = 0x01;
+  
+  //EnableInterrupts;
+  
+  for(;;)
+  {
+    errorflag = CANSendFrame(ST_ID_100, 0x00, (sizeof(txbuff)-1), txbuff);
+    
+    delay(20000);
+  }
+}
+
+unsigned char CANSendFrame(unsigned long id, unsigned char priority, unsigned char length, unsigned char *txdata)
+{ 
+  unsigned char txbuffer;
+  int index=0;
+   
+  if (!CANTFLG)
+  {
+    return ERR_BUFFER_FULL;
+  }/* Is Transmit Buffer full?? */
+    
+  
+  CANTBSEL = CANTFLG; /* Select lowest empty buffer */
+  txbuffer = CANTBSEL; /* Backup selected buffer */
+  
+  /* Load Id to IDR Register */
+  *((unsigned long *) ((unsigned long)(&CANTXIDR0))) = id;
+  
+  for (index=0; index<length; index++) {
+    *(&CANTXDSR0 + index) = txdata[index]; // Load data to Tx buffer
+                                            // Data Segment Registers
+  }
+  
+  CANTXDLR = length; // Set Data Length Code 
+  CANTXTBPR = priority; // Set Priority 
+  CANTFLG = txbuffer; // Start transmission 
+  
+  while ( (CANTFLG & txbuffer) != txbuffer); // Wait for Transmission completion
+
+}
+
+
+void delay(int milliseconds)
+{
+    long pause;
+    clock_t now,then;
+
+    pause = milliseconds*(CLOCKS_PER_SEC/1000);
+    now = then = clock();
+    while( (now-then) < pause )
+        now = clock();
+}
+
+void interrupt CANRxISR(void)
+{
+  unsigned char length, index;
+  unsigned char rxdata[8];
+  
+  length = (CANRXDLR&0x0F);
+  for (index=0; index<length; index++) 
+  {
+    rxdata[index] = *(&CANRXDSR0 + index); // Get received data
+  }
+
+  CANRFLG = 0x01; // Clear RXF
+}
