4cb7c76e573853184b8581a703d9c3104b3c9c8d
dinkar-sharma <dsharma4187@conestogac.on.ca>
2017-06-05 11:38:02 -0400

    Added car_controller source code

--- source/mscan_module/Sources/mscan.h
+++ source/mscan_module/Sources/mscan.h
@@ -1,18 +1,42 @@
-/* Acceptance Code Definitions */
-#define ACC_CODE_ID100 0x2000
-#define ACC_CODE_ID100_HIGH ((ACC_CODE_ID100&0xFF00)>>8)
-#define ACC_CODE_ID100_LOW (ACC_CODE_ID100&0x00FF)
+#ifndef _MSCAN_H
+#define _MSCAN_H
 
-/* Mask Code Definitions */
-#define MASK_CODE_ST_ID 0x0007
-#define MASK_CODE_ST_ID_HIGH ((MASK_CODE_ST_ID&0xFF00)>>8)
-#define MASK_CODE_ST_ID_LOW (MASK_CODE_ST_ID&0xFF)
+/*CANTCL0 register*/
+#define CANCTL1_INITRQ_MASK     0x01  
 
-#define ST_ID_100 0x20000000
+/*CANTCL1 register*/
+// turn on MSCAN module
+#define CANCTL1_CANE_MASK       0x80
+// init mode request to enter init mode
+#define CANCTL1_INITAK_MASK     0x01
+// enable bus clock = 8MHz
+#define CANCTL1_CLKSRC_MASK     0x40
+#define CANCTL1_INIT (CANCTL1_CANE_MASK & CANCTL1_CLKSRC_MASK)
 
-#define NO_ERR 0 
+/*CANBTR0 register*/
+// CAN synchronization jump width = 1
+// CAN prescaler value = 8
+#define CANBTR0_INIT            0x07
 
-#define ERR_BUFFER_FULL 1
+/*CANBTR1 register*/
+// CAN time segment 1 value = 4
+// CAN time segment 2 value = 3
+// CAN Tq = 1
+#define CANBTR1_INIT            0x23
 
-#define SET_BITS(port,mask) (port)|=(mask)
-#define CLR_BITS(port,mask) ((port)&=((~mask)&0xFF))
+#define NO_ERR                  0 
+
+#define ERR_BUFFER_FULL         1
+
+
+
+
+typedef struct msgFrame
+{
+  unsigned long id;
+  unsigned char priority;
+  unsigned char length;
+  unsigned char *txdata;
+}*CANmsg_t;
+
+#endif
