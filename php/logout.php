f306006bf1c43ff423980ae69edef9af5880fc2e
Anas Yassin <anasyaseen1@hotmail.com>
2017-06-19 14:49:23 -0400

    updated webpages

--- php/logout.php
+++ php/logout.php
@@ -1,7 +1,5 @@
 <?php
 	session_start();
-	echo "my nigga";
-	header("Location: ../index.html")
 	session_destroy();
-	//echo"You have been logged out. Click <a href='login.html'>here</a> to log in again";
+	header("Location: ../index.html");
 ?>
