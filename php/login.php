2a8e2272804a5124631628c215e9c30e4a7cfde1
Anas Yassin <anasyaseen1@hotmail.com>
2017-06-26 15:14:43 -0400

    linked database to login page

--- php/login.php
+++ php/login.php
@@ -5,12 +5,27 @@ $username = $_POST['username'];
 $password = $_POST['password'];
 
 if($username&&$password){
-	$_SESSION['username']=$username;
-	//echo"<p>Congratulations</p>";
-	//echo "<p>Please click <a href=\"members.php\">here</a> to be taken to our members only page</p>";
+	//$_SESSION['username']=$username;
+	$db = new PDO(
+		'mysql:host=127.0.0.1;dbname=elevator',
+		'root',
+		'');
+	$authenticated = FALSE;
+	$rows = $db->query('SELECT * FROM users ORDER BY userID');
+	foreach ($rows as $row){
+		if ($username == $row[1] && $password == $row[2]){
+			$authenticated = TRUE;
+
+		}
+	}
+	if ($authenticated == TRUE){
+		$_SESSION['username']=$username;
+	
 	header("Location: ../elevator_control.html");
-} else{
-	echo"<p>Please enter a username and password</p>";
+	}
+	else{
+		echo"<p>Please enter a username and password</p>";
+	}
 }
 
 ?>
