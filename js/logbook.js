bd49f59c0aaf4ddf39d9a20f9c5bf2906d532dfc
Dinkar Sharma <dsharma4187@conestogac.on.ca>
2017-07-06 00:55:44 -0400

    worked on pages. added javascript

---
+++ js/logbook.js
@@ -0,0 +1,12 @@
+var today = new Date();
+var	hourNow = today.getFullYear();
+var birthdate = new Date("Aug 4, 1996 12:00:00");
+
+var age = today.getTime() - birthdate.getTime();
+
+age = Math.floor(age/31556900000);
+
+msg = "<p>My age is " + age + " </p>";
+
+var element = document.getElementById('age');
+element.innerHTML = msg;
