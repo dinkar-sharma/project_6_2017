cb635fb1cf97b9436963ebdbd0060f2321c62db2
Dinkar Sharma <dsharma4187@conestogac.on.ca>
2017-07-05 22:15:46 -0400

    updated index page

--- js/index.js
+++ js/index.js
@@ -1,7 +1,5 @@
-function myMap() {
-	var myCenter = new google.maps.LatLng(41.878114, -87.629798);
-	var mapProp = {center:myCenter, zoom:12, scrollwheel:false, draggable:false, mapTypeId:google.maps.MapTypeId.ROADMAP};
-	var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
-	var marker = new google.maps.Marker({position:myCenter});
-	marker.setMap(map);
-}
+$('#myCarousel').carousel({
+  interval: 3000
+}).on('slide.bs.carousel', function () {
+  document.getElementById('bg-video').pause();
+})
