4c4dd24fca0f51c45f4431825e760f2b7ba6bd70
mikeakl <mikeakl@users.noreply.github.com>
2017-07-10 13:27:12 -0400

    Adding age to logbooks

--- js/logbook.js
+++ js/logbook.js
@@ -1,12 +1,42 @@
 var today = new Date();
 var	hourNow = today.getFullYear();
+
 var dinkarBirthdate = new Date("Aug 4, 1996 12:00:00");
+var anasBirthdate = new Date("Feb 21, 1994 12:00:00");
+var mikeBirthdate = new Date("July 7, 1989 12:00:00");
 
-var age = today.getTime() - dinkarBirthdate.getTime();
+var dEle = document.getElementById('dinkar');
+var aEle = document.getElementById('anas');
+var mEle = document.getElementById("mike");
 
-age = Math.floor(age/31556900000);
 
-msg = "<p>My age is " + age + " </p>";
+function bDayFunc(bDay){
 
-var element = document.getElementById('age');
-element.innerHTML = msg;
+	var age = today.getTime() - bDay.getTime();
+
+	age = Math.floor(age/31556900000);
+
+	msg = "<p>My age is " + age + " </p>";
+	
+	//return msg; 
+}
+
+if ( dEle.innerHTML == 'dinkar'){
+	bDayFunc(dinkarBirthdate);
+	dEle.innerHTML = msg;
+} 
+if ( aEle.innerHTML == 'anas') {
+	bDayFunc(anasBirthdate);
+	aEle.innerHTML = msg;
+} 
+if( mEle.innerHTML == 'mike'){
+	bDayFunc(mikeBirthdate);
+	mEle.innerHTML = msg;
+}
+
+
+
+
+
+
+
