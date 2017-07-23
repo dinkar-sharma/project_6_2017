626fec77c4c6937bd3fdf1476e421b313dd2b2af
dinkar-sharma <dsharma4187@conestogac.on.ca>
2017-07-23 16:27:45 -0400

    Updated GUI page

--- js/elevator_control.js
+++
@@ -1,5 +0,0 @@
-var usernameSubmit = document.getElementById('username');
-var welcomeMsg = document.getElementById('welcome');
-var debugPanel = document.getElementById('debug-content');
-
-document.getElementById('welcome').innerHTML = usernameSubmit;
