cb9d1ac560e6fed7aa025418532e906f2d19632e
dinkar-sharma <dsharma4187@conestogac.on.ca>
2017-07-29 19:39:41 -0400

    Using AJAX to display database and request elevator

---
+++ php/display_database.php
@@ -0,0 +1,54 @@
+<?php 
+
+	function authorized_users_display($dbConn)
+	{
+	    $query = 'SELECT * FROM authorized_users ORDER BY userID LIMIT 10';
+	    $rows = $dbConn->query($query);
+	    foreach ($rows as $row) 
+	    {
+	        echo "<tr>";
+	        for ($i=0; $i < sizeof($row)/2 ; $i++) 
+	        { 
+	            echo "<td>".$row[$i]."</td>";
+	        }
+	        echo "</tr>";
+	    }
+	}
+
+	function elevator_network_display($dbConn)
+	{
+	    $query = 'SELECT * FROM (SELECT * FROM elevator_network ORDER BY timeID DESC LIMIT 10) sub ORDER BY timeID ASC';
+	    $rows = $dbConn->query($query);
+	    foreach ($rows as $row) 
+	    {
+	        echo "<tr>";
+	        for ($i=0; $i < sizeof($row)/2 ; $i++) 
+	        { 
+	            echo "<td>".$row[$i]."</td>";
+	        }
+	        echo "</tr>";
+	    }
+	}
+
+	function connect_to_database()
+	{
+		try 
+		{
+		    $db = new PDO(
+		    'mysql:host=127.0.0.1;dbname=elevator_project_2017',
+		    'root',
+		    '');
+		} 
+		catch (Exception $e) 
+		{
+		    echo "Error connecting to database: " .$e->getMessage();
+		}
+
+		return $db;
+	}
+
+
+	$dbConn = connect_to_database();
+	elevator_network_display($dbConn);
+	authorized_users_display($dbConn);
+ ?>
