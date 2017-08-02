aeb829992272ab86d49630a2f7b6d91efa7bed91
mikeakl <mikeakl@gmail.com>
2017-08-02 11:31:07 -0400

    trying to update elevator_network table to have currentFloor

--- php/elevator_control_ajax.php
+++ php/elevator_control_ajax.php
@@ -5,6 +5,16 @@
         echo "<p>You are not authorized users. Click <a href=../request_access.html> here to sign up.</p>"; 
         die();
     }
+	
+	function read_current_floor($dbConn)
+	{
+		echo "HELLO";
+		//$dbConn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		$test = $dbConn->query('SELECT currentFloor FROM cFloor_table');
+	    var_dump($test);
+		//echo $test;
+		return 0; //$test; //$rows;
+	}
 
 	function elevator_network_display($dbConn)
 	{
@@ -23,7 +33,7 @@
 
 
 	function elevator_network_write($dbConn, $requestedFloor, $doorState, $controllerType)
-	{
+	{	
 	    try
 	    {
 	    	$query = 'INSERT INTO elevator_network(nodeID, requestedFloor, controllerType, doorState, currentFloor, dateID, timeID) VALUES (100, :requestedFloor, :controllerType, :doorState, :currentFloor, :dateID, :timeID)';
@@ -33,8 +43,12 @@
 		    $curr_date = $curr_date_query->fetch(PDO::FETCH_ASSOC);
 		    $curr_time_query = $dbConn->query('SELECT CURRENT_TIME()');
 		    $curr_time = $curr_time_query->fetch(PDO::FETCH_ASSOC);
-		    $currentFloor = $_POST['currentFloor'] ?? '';
-
+			$currentFloor = read_current_floor($dbConn);
+		    //$curr_floor_query = $dbConn->query('SELECT requestedFloor FROM cFloor_table');
+		    //$currentFloor = $curr_floor_query->fetch(PDO::FETCH_ASSOC);
+			
+			//echo $currentFloor;
+			
 		    $params = [
 		        'requestedFloor' => $requestedFloor,
 		        'controllerType' => $controllerType,
@@ -44,9 +58,78 @@
 		        'timeID' => $curr_time['CURRENT_TIME()']
 		    ];
 		   
-		    if(!$statement->execute($params)) 
+		    if(!($statement->execute($params)))
 		    {
 		       throw new Exception("elevator_network_write: Error executing statement");
+		    }
+		    $dbConn->commit();
+	    
+	    } 
+	    catch (Exception $e) 
+	    {
+	    	$dbConn->rollBack();
+	    	echo "Failed: " .$e->getMessage();
+	    }
+	}
+	
+	function CAN_network_write($dbConn, $requestedFloor, $doorState)
+	{		
+	    try
+	    {
+	    	$query = 'INSERT INTO CAN_network(CAN_ID, requestedFloor, doorState, currentFloor, dateID, timeID) VALUES (100, :requestedFloor, :doorState, :currentFloor, :dateID, :timeID)';
+	    	$dbConn->beginTransaction();
+		    $statement = $dbConn->prepare($query);
+		    $curr_date_query = $dbConn->query('SELECT CURRENT_DATE()');
+		    $curr_date = $curr_date_query->fetch(PDO::FETCH_ASSOC);
+		    $curr_time_query = $dbConn->query('SELECT CURRENT_TIME()');
+		    $curr_time = $curr_time_query->fetch(PDO::FETCH_ASSOC);
+			//$curr_floor_query = $dbConn->query('SELECT requestedFloor FROM cFloor_table');
+		    $currentFloor = read_current_floor($dbConn);
+			echo $currentFloor;
+			
+		    $params = [
+		        'requestedFloor' => $requestedFloor,
+		        'doorState' => $doorState,
+		        'currentFloor' => $currentFloor,
+		        'dateID' => $curr_date['CURRENT_DATE()'],
+		        'timeID' => $curr_time['CURRENT_TIME()']
+		    ];
+		   
+		    if(!$statement->execute($params)) 
+		    {
+		       throw new Exception("CAN_network_write: Error executing statement");
+		    }
+		    $dbConn->commit();
+	    
+	    } 
+	    catch (Exception $e) 
+	    {
+	    	$dbConn->rollBack();
+	    	echo "Failed: " .$e->getMessage();
+	    }
+
+	}
+	
+	function queue_table_write($dbConn, $requestedFloor, $doorState)
+	{
+	
+	    try
+	    {
+	    	$query = 'INSERT INTO queue_table(nodeID, requestedFloor, doorState, currentFloor) VALUES (100, :requestedFloor, :doorState, :currentFloor)';
+	    	$dbConn->beginTransaction();
+		    $statement = $dbConn->prepare($query);
+		    //$curr_floor_query = $dbConn->query('SELECT requestedFloor FROM cFloor_table');
+		    $currentFloor = read_current_floor($dbConn);
+
+		    $params = [
+		        'requestedFloor' => $requestedFloor,
+		        'doorState' => $doorState,
+		        'currentFloor' => $currrentFloor
+		    ];
+		   
+		    if(!$statement->execute($params)) 
+		    {
+		       throw new Exception("queue_table_write: Error executing statement");
 		    }
 		    $dbConn->commit();
 	    
@@ -122,5 +205,7 @@
 	{
 		$db = connect_to_database();
 		elevator_network_write($db, $requestedFloor, $doorState, $controllerType);
+		CAN_network_write($db, $requestedFloor, $doorState);
+		queue_table_write($db, $requestedFloor, $doorState);
 	}
 ?>
